bece6c271f128a7856335c859d4aa546b7091d41
Isaac <isaac.brasil@escolarmanager.com.br>
2022-11-07 09:10:39 -0300

    finalizando app

--- lib/app/components/currency_box.dart
+++ lib/app/components/currency_box.dart
@@ -1,10 +1,21 @@
+import 'package:conversor_curso/app/models/currency_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class CurrencyBox extends StatelessWidget {
-  const CurrencyBox({super.key});
+  final List<CurrencyModel> items;
+  final CurrencyModel? selectedItem;
+  final TextEditingController controller;
+  final void Function(CurrencyModel? model) onChanged;
+
+  const CurrencyBox(
+      {super.key,
+      required this.items,
+      required this.controller,
+      required this.onChanged,
+      required this.selectedItem});
 
   @override
   Widget build(BuildContext context) {
@@ -15,40 +26,18 @@ class CurrencyBox extends StatelessWidget {
             flex: 1,
             child: SizedBox(
               height: 56,
-              child: DropdownButton(
+              child: DropdownButton<CurrencyModel>(
                 iconEnabledColor: Colors.amber,
                 isExpanded: true,
+                value: selectedItem,
                 underline: Container(
                   height: 1,
                   color: Colors.amber,
                 ),
-                items: [
-                  DropdownMenuItem(
-                      value: 1,
-                      child: Text(
-                        'Real',
-                        overflow: TextOverflow.ellipsis,
-                      )),
-                  DropdownMenuItem(
-                      value: 1,
-                      child: Text(
-                        'Dolar',
-                        overflow: TextOverflow.ellipsis,
-                      )),
-                  DropdownMenuItem(
-                      value: 1,
-                      child: Text(
-                        'Euro',
-                        overflow: TextOverflow.ellipsis,
-                      )),
-                  DropdownMenuItem(
-                      value: 1,
-                      child: Text(
-                        'Bitcoin',
-                        overflow: TextOverflow.ellipsis,
-                      )),
-                ],
-                onChanged: (value) {},
+                items: items
+                    .map((e) => DropdownMenuItem(value: e, child: Text(e.name)))
+                    .toList(),
+                onChanged: onChanged,
               ),
             )),
         SizedBox(
@@ -57,6 +46,7 @@ class CurrencyBox extends StatelessWidget {
         Expanded(
           flex: 2,
           child: TextField(
+            controller: controller,
             decoration: InputDecoration(
                 focusedBorder: UnderlineInputBorder(
                     borderSide: BorderSide(color: Colors.amber)),
--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,10 +1,28 @@
+import 'package:conversor_curso/app/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import '../components/currency_box.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final TextEditingController toText = TextEditingController();
+  final TextEditingController fromText = TextEditingController();
+
+  HomeController? homeController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    homeController = HomeController(toText: toText, fromText: fromText);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +37,30 @@ class HomeView extends StatelessWidget {
               height: 150,
             ),
             SizedBox(height: 50),
-            CurrencyBox(),
+            CurrencyBox(
+                selectedItem: homeController?.toCurrency,
+                controller: toText,
+                items: homeController!.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    homeController?.toCurrency = model;
+                  });
+                }),
             SizedBox(height: 10),
-            CurrencyBox(),
+            CurrencyBox(
+                selectedItem: homeController?.fromCurrency,
+                controller: fromText,
+                items: homeController!.currencies,
+                onChanged: (model) {
+                  setState(() {
+                    homeController?.fromCurrency = model;
+                  });
+                }),
             SizedBox(height: 50),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                homeController?.convert();
+              },
               child: Text('CONVERTER'),
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.amber),
